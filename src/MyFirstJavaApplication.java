4a782d7fb7f7acdd25fedb57af1fd279c014571f
Sam Black <sam.black@lubbockisd.org>
2023-08-30 10:17:45 -0500

    Added notes on escape sequences and print.

--- src/MyFirstJavaApplication.java
+++ src/MyFirstJavaApplication.java
@@ -1,5 +1,18 @@
-public class Main {
+public class MyFirstJavaApplication {
+// This is a comment describing the purpose of this Java class
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.print("Check this out.");
+        System.out.println("Wow how cool.");
+        //printf -- formatted prints
+
+        // Escape sequences -- all begin with a \
+        System.out.println("howdy\nfolks");// newline character
+        System.out.println("what's\tgoing on?\b");//tab character
+                                            //backspace character
+
     }
+
+
 }
